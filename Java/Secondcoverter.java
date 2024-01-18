2002cfc8a3fb59d1bc6d6007f677e35b2c4e2b01
Noah <106123749+nbran3@users.noreply.github.com>
2024-01-18 13:11:20 -0500

    Create Secondcoverter.java

---
+++ Java/Secondcoverter.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hours1 = scanner.nextInt();
+        int minutes1 = scanner.nextInt();
+        int seconds1 = scanner.nextInt();
+
+        int hours2 = scanner.nextInt();
+        int minutes2 = scanner.nextInt();
+        int seconds2 = scanner.nextInt();
+
+
+        int totalSeconds1 = hours1 * 3600 + minutes1 * 60 + seconds1;
+        int totalSeconds2 = hours2 * 3600 + minutes2 * 60 + seconds2;
+
+
+        int secondsDifference = totalSeconds2 - totalSeconds1;
+
+        System.out.println(secondsDifference);
+
+        scanner.close();
+    }
+}
